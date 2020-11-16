3bae9856b5b4f311f03db5a6beaa792c83c7f7c6
Andrew Klychkov <aaklychkov@mail.ru>
2020-11-16 14:14:21 +0300

    Add check_doc_section function

--- module_reviewer.py
+++ module_reviewer.py
@@ -11,6 +11,9 @@ import sys
 from yaml import load
 
 
+LINE_MAX_LEN = 79
+
+
 def check_cli_args(arg_list):
     """Checks CLI arguments"""
     # Maybe later it'll be argparser
@@ -22,9 +25,9 @@ def check_cli_args(arg_list):
 def get_sections_to_check(module_path):
     """Read a module file and extracts section to check"""
 
-    DOCUMENTATION = []
-    EXAMPLES = []
-    RETURN = []
+    documentation = []
+    examples = []
+    returns = []
 
     is_in_doc_section = False
     is_in_examples_section = False
@@ -45,56 +48,121 @@ def get_sections_to_check(module_path):
 
                 continue
 
-            # Start to extract the DOCUMENTATION section
+            # Start to extract the documentation section
             if 'DOCUMENTATION' in line:
                 is_in_doc_section = True
                 continue
 
-            # Start to extract the EXAMPLES section
+            # Start to extract the examples section
             elif 'EXAMPLES' in line:
                 is_in_examples_section = True
                 continue
 
-            # Start to extract the RETURN section
+            # Start to extract the return section
             elif 'RETURN' in line:
                 is_in_return_section = True
                 continue
 
             # Put the line in an appropriate list
             if is_in_doc_section:
-                DOCUMENTATION.append(line)
+                documentation.append(line)
 
             elif is_in_examples_section:
-                EXAMPLES.append(line)
+                examples.append(line)
 
             elif is_in_return_section:
-                RETURN.append(line)
+                returns.append(line)
 
-    if DOCUMENTATION:
-        DOCUMENTATION = ''.join(DOCUMENTATION)
+    if documentation:
+        documentation = ''.join(documentation)
 
-    if EXAMPLES:
-        EXAMPLES = ''.join(EXAMPLES)
+    if examples:
+        examples = ''.join(examples)
 
-    if RETURN:
-        RETURN = ''.join(RETURN)
+    if returns:
+        returns = ''.join(returns)
 
     try:
-        DOCUMENTATION = load(DOCUMENTATION)
+        documentation = load(documentation)
     except AttributeError:
         pass
 
     try:
-        EXAMPLES = load(EXAMPLES)
+        examples = load(examples)
     except AttributeError:
         pass
 
     try:
-        RETURN = load(RETURN)
+        returns = load(returns)
     except AttributeError:
         pass
 
-    return DOCUMENTATION, EXAMPLES, RETURN
+    return documentation, examples, returns
+
+
+def check_doc_section(doc, report):
+    """Check the documentation section"""
+
+    # If there is no the documentation block, exit
+    if not doc:
+        raise AttributeError('"DOCUMENTATION" section is not provided, '
+                             'nothing to parse, exit')
+
+    # Check if short_description starts with a capital letter
+    # and does not end with a dot
+    if not doc['short_description'][0].isupper():
+        report.append("module's short_description: does not start "
+                      "with a capital letter")
+
+    if doc['short_description'][-1] == '.':
+        report.append("module's short_description: ends with a dot")
+
+    # Check if every line of description starts with a capital letter
+    # and ends with a dot
+    for n, line in enumerate(doc['description']):
+        # Starts with uppercase?
+        if not line[0].isupper():
+            report.append("module's description: line %s does not start "
+                          "with a capital letter" % (n + 1))
+
+        # Ends with a dot?
+        if line[-1] != '.':
+            report.append("module's description: line %s does not "
+                          "end with a dot" % (n + 1))
+
+        # Uses a marker with http?
+        if needs_marker(line, 'http', 'U'):
+            report.append("module's description: has URLs "
+                          "used without U() marker")
+
+        # Check length
+        line_len = len(line)
+        if line_len > LINE_MAX_LEN:
+            report.append("module's description: line %s is %s characters "
+                          "long which is longer than %s. Would not be better "
+                          "to split them?" % (n, line_len, LINE_MAX_LEN))
+
+
+def needs_marker(string, pattern, marker):
+    """Check if a substring is following a certain marker.
+
+    Args:
+      string (str) - string to search in
+      pattern (str) - pattern to search for
+      marker (str) - marker to be followed by pattern, can be U, C, M, etc.
+    """
+
+    pattern_start_pos = string.find(pattern)
+
+    if pattern_start_pos != -1:
+        if pattern_start_pos < 2:
+            return True
+
+        marker_check_pos = pattern_start_pos - 2
+        if string[marker_check_pos] != marker:
+            return True
+
+    return False
 
 
 def main():
@@ -103,12 +171,17 @@ def main():
     check_cli_args(sys.argv)
 
     # Extract sections
-    DOCUMENTATION, EXAMPLES, RETURN = get_sections_to_check(sys.argv[1])
+    documentation, examples, returns = get_sections_to_check(sys.argv[1])
 
-    # If there is no DOCUMENTATION block, exit
-    if not DOCUMENTATION:
-        raise AttributeError('"DOCUMENTATION" section is not provided, '
-                             'nothing to parse, exit')
+    # Create a report object
+    report = []
+
+    # Check the documentation section
+    check_doc_section(documentation, report)
+
+    # TODO: Debug
+    for line in report:
+        print(line)
 
 
 if __name__ == '__main__':
